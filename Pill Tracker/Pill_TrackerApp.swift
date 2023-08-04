2eaf334adf907c14a24df5738fd0ac227a708426
Hamza Rafique Azad <hamza.rafiqazad@gmail.com>
2023-08-04 10:19:16 +0500

    Initial Commit

---
+++ Pill Tracker/Pill_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pill_TrackerApp.swift
+//  Pill Tracker
+//
+//  Created by Hamza Rafique Azad on 4/8/23.
+//
+
+import SwiftUI
+
+@main
+struct Pill_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
